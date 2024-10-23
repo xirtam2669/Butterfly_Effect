f5bf37e0268c14fe9917098fe8e9fd8eb3211739
Alex Messham <52509289+xirtam2669@users.noreply.github.com>
2024-10-23 08:06:55 -0400

    Update aes.py

--- aes.py
+++ aes.py
@@ -4,29 +4,33 @@ import binascii
 import os
 import sys
 
+def to_csharp_byte_array(data):
+    return ', '.join(f'0x{byte:02x}' for byte in data)
+
 shellcode_file = sys.argv[1]
 with open(shellcode_file, "rb") as f:
     shellcode = f.read()
 
+# AES key
 key = b'1234567890123456'  # 16-byte key for AES-128
 
-# AES Initialization Vector (IV) - must be 16 bytes long for AES
+# AES Initialization Vector (IV) - must be 16 bytes long for AES-128
 iv = b'1234567890123456'  # Static IV
-
-key_raw = binascii.hexlify(key)
-iv_raw = binascii.hexlify(iv)
 
 # Create AES cipher in CBC mode
 cipher = AES.new(key, AES.MODE_CBC, iv)
 
-padded_shellcode = pad(shellcode, AES.block_size) #Make sure this actually works
+#Pad shellcode
+padded_shellcode = pad(shellcode, AES.block_size)
 
 # Encrypt the padded shellcode
 encrypted_shellcode = cipher.encrypt(padded_shellcode)
 
-# Convert encrypted shellcode to hexadecimal for display
-encrypted_shellcode_hex = binascii.hexlify(encrypted_shellcode)
+#Uncomment if you'd plan to change key/IV, as you'll need this to update butterfly_effect
+#print("byte[] key = new byte[] { " + csharp_byte_array_key + " };")
+#print("byte[] iv = new byte[] { " + csharp_byte_array_iv + " };")
 
-print(f"IV (Hex): {iv_raw}")
-print(f"Key (Hex): {key_raw}")
-print(f"Encrypted Shellcode (Hex): {encrypted_shellcode_hex.decode()}")
+with open("cipher.bin", "wb") as f:
+        f.write(encrypted_shellcode)
+
+print("Encrypted shellcode has been written to file: cipher.bin")
